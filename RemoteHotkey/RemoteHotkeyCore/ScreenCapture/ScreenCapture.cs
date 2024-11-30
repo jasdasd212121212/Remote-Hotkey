f73ce34a804c3d1ed8421d1f33ba7d978397aa11
Belugacccc <149381835+jasdasd212121212@users.noreply.github.com>
2024-11-30 19:22:19 +0300

    Adden automatic resolution recorgonizing

--- RemoteHotkey/RemoteHotkeyCore/ScreenCapture/ScreenCapture.cs
+++ RemoteHotkey/RemoteHotkeyCore/ScreenCapture/ScreenCapture.cs
@@ -1,17 +1,20 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.Text;
 
 namespace RemoteHotkey.ScreenCapture;
 
 public class ScreenCapture
 {
+    private int _width;
+    private int _height;
+
     private Bitmap _screen;
     private const string FILE_NAME = "temp.jpeg";
 
     public ScreenCapture()
     {
-        _screen = new Bitmap(1920, 1080);
+        GetScreen(ref _width, ref _height);
+        _screen = new Bitmap(_width, _height);
     }
 
     ~ScreenCapture()
@@ -21,7 +24,7 @@ public class ScreenCapture
 
     public byte[] CaptureScreen()
     {
-        Rectangle bounds = new Rectangle(0, 0, 1920, 1080);
+        Rectangle bounds = new Rectangle(0, 0, _width, _height);
 
         using (Bitmap captureBitmap = new Bitmap(bounds.Width, bounds.Height))
         {
@@ -34,5 +37,28 @@ public class ScreenCapture
         }
 
         return File.ReadAllBytes(FILE_NAME);
+    }
+
+    private void GetScreen(ref int width, ref int height)
+    {
+        var managementScope = new System.Management.ManagementScope();
+        managementScope.Connect();
+        var q = new System.Management.ObjectQuery("SELECT CurrentHorizontalResolution, CurrentVerticalResolution FROM Win32_VideoController");
+        var searcher = new System.Management.ManagementObjectSearcher(managementScope, q);
+        var records = searcher.Get();
+
+        int rr = records.Count;
+
+        foreach (var record in records)
+        {
+            if (!int.TryParse(record.GetPropertyValue("CurrentHorizontalResolution").ToString(), out width))
+            {
+                throw new Exception("Throw some exception");
+            }
+            if (!int.TryParse(record.GetPropertyValue("CurrentVerticalResolution").ToString(), out height))
+            {
+                throw new Exception("Throw some exception");
+            }
+        }
     }
 }
