0d024e76efdbf466710f3c056b89971af9481038
jasdasd212121212 <149381835+jasdasd212121212@users.noreply.github.com>
2025-04-21 19:47:15 +0300

    Remove trash

--- ControllingClient/RemoteHotkey/RemoteHotkeyCore/ScreenCapture/Helpers/DataCompress/CustomScreenCompressor.cs
+++ ControllingClient/RemoteHotkey/RemoteHotkeyCore/ScreenCapture/Helpers/DataCompress/CustomScreenCompressor.cs
@@ -12,15 +12,11 @@ public class CustomScreenCompressor : IScreenDataCompressor
         {
             using (var compressedStream = new MemoryStream())
             {
-                // setting the leaveOpen parameter to true to ensure that compressedStream will not be closed when compressorStream is disposed
-                // this allows compressorStream to close and flush its buffers to compressedStream and guarantees that compressedStream.ToArray() can be called afterward
-                // although MSDN documentation states that ToArray() can be called on a closed MemoryStream, I don't want to rely on that very odd behavior should it ever change
                 using (var compressorStream = new DeflateStream(compressedStream, CompressionLevel.Fastest, true))
                 {
                     uncompressedStream.CopyTo(compressorStream);
                 }
 
-                // call compressedStream.ToArray() after the enclosing DeflateStream has closed and flushed its buffer to compressedStream
                 compressedBytes = compressedStream.ToArray();
             }
         }
